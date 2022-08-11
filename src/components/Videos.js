0d5894d672ca6c44399c2e6f2d5fbc72e4455641
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-08-12 00:00:12 +0600

    video card grid ui update

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -4,8 +4,17 @@ import Video from './Video';
 export default function Videos() {
   return (
     <div>
-        <Video />
-        <Video />
+      <div className="row">
+        <div class="col-lg-4 col-md-12">
+          <Video />
+        </div>
+        <div class="col-lg-4 col-md-12">
+          <Video />
+        </div>
+        <div class="col-lg-4 col-md-12">
+          <Video />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
