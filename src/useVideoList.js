2c06ef9be625aa926580b9369ca8168e0e39a7b0
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-09-13 01:28:03 +0600

    first api intg testing

--- src/useVideoList.js
+++ src/useVideoList.js
@@ -1,28 +1,43 @@
 import { useEffect,useState } from 'react';
-import { getDatabase, orderByKey, query, ref,get, startAt, limitToFirst } from 'firebase/database';
+// import { getDatabase, orderByKey, query, ref,get, startAt, limitToFirst } from 'firebase/database';
 
 export default function useVideoList(page) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [videos, setVideos] = useState([]);
-    const [hasMore, setHasMore] = useState(true);
+    // const [hasMore, setHasMore] = useState(true);
   useEffect(() => {
     async function fetchData() {
-        const db = getDatabase();
-        const videoRef = ref(db, 'videos');
-        const videoQuery = query(videoRef, orderByKey(), startAt("" + page),limitToFirst(8));
+        // const db = getDatabase();
+        // const videoRef = ref(db, 'videos');
+        // const videoQuery = query(videoRef, orderByKey(), startAt("" + page),limitToFirst(8));
+        
         try {
-            setError(false)
+            // setError(false)
+            // setLoading(true);
+            // const result = await get(videoQuery);
+            // setLoading(false);
+            // if(result.exists()){
+            //     setVideos((prev)=>{
+            //         return [ ...prev, ...Object.values(result.val())];
+            //     })
+            // }else{
+            //     setHasMore(false);
+            // }
+            let url = "http://127.0.0.1:8000/quiz.categorycreataorget";
+            const requestOptions = {
+              method: "GET",
+              headers: { "Content-Type": "application/json" },
+            };
             setLoading(true);
-            const result = await get(videoQuery);
-            setLoading(false);
-            if(result.exists()){
-                setVideos((prev)=>{
-                    return [ ...prev, ...Object.values(result.val())];
-                })
-            }else{
-                setHasMore(false);
-            }
+            await fetch(url, requestOptions)
+              .then((res) => res.json())
+              .then((response) => {
+                // console.log('res_new',response);
+                setError(false);
+                setLoading(false);
+                setVideos(response.data);
+              });
         } catch (error) {
             setLoading(false);
             setError(error);
@@ -30,5 +45,5 @@ export default function useVideoList(page) {
     }
     fetchData();
   } , [page]);
-    return {loading, error, videos, hasMore}; 
+    return {loading, error, videos}; 
 }
